ddb7fe7ae9036f6f5e8020965133977369c2a6ab
Gourav <gourav2824@gmail.com>
2023-06-04 00:41:04 +0530

    Add: class with primary constructor and fields

---
+++ src/main/kotlin/classes/Main.kt
@@ -0,0 +1,7 @@
+package classes
+
+fun main() {
+    val person = Person("John", "Doe")
+    println(person.firstName)
+    println(person.lastName)
+}
---
+++ src/main/kotlin/classes/Person.kt
@@ -0,0 +1,4 @@
+package classes
+
+// Class having a primary constructor and two fields
+class Person(val firstName: String, val lastName: String)
