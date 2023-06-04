e4a507ec1683ab5dba2bf2f97e09c75a0c319ba6
Gourav <gourav2824@gmail.com>
2023-06-04 23:27:13 +0530

    Add: secondary constructor

--- src/main/kotlin/classes/Main.kt
+++ src/main/kotlin/classes/Main.kt
@@ -1,10 +1,16 @@
 package classes
 
 fun main() {
+    val empty = Empty()
+    println(empty)
+
     val person = Person("John", "Doe")
     println(person.firstName)
     println(person.lastName)
+    println(person.middleName)
 
-    val empty = Empty()
-    println(empty)
+    val person1 = Person("John", "Wick", "Doe")
+    println(person1.firstName)
+    println(person1.lastName)
+    println(person1.middleName)
 }
--- src/main/kotlin/classes/Person.kt
+++ src/main/kotlin/classes/Person.kt
@@ -1,4 +1,11 @@
 package classes
 
 // Class having a primary constructor and two fields
-class Person(val firstName: String, val lastName: String)
+class Person(val firstName: String, val lastName: String) {
+    var middleName: String? = null
+
+    // Secondary constructor
+    constructor(_firstName: String, _middleName: String, _lastName: String) : this(_firstName, _lastName) {
+        middleName = _middleName
+    }
+}
